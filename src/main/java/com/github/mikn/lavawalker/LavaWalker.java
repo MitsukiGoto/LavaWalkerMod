07014231a48becd6cac30ddcc7c71fa3c6ee9b46
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2024-07-18 00:12:41 +0900

    add an enchantment that does nothing

--- src/main/java/com/github/mikn/lavawalker/LavaWalker.java
+++ src/main/java/com/github/mikn/lavawalker/LavaWalker.java
@@ -22,15 +22,9 @@
 package com.github.mikn.lavawalker;
 
 import com.github.mikn.lavawalker.config.LavaWalkerConfig;
-import com.github.mikn.lavawalker.init.BlockInit;
-import com.github.mikn.lavawalker.init.EnchantmentInit;
-import com.github.mikn.lavawalker.init.ItemInit;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -43,9 +37,6 @@ public class LavaWalker implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Registry.register(BuiltInRegistries.ENCHANTMENT, new ResourceLocation(LavaWalker.MODID, "lava_walker"), EnchantmentInit.LAVA_WALKER);
-        Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(LavaWalker.MODID, "modded_obsidian"), BlockInit.MODDED_OBSIDIAN);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(LavaWalker.MODID, "modded_obsidian"), ItemInit.MODDED_OBSIDIAN);
     }
     static {
         HOLDER = AutoConfig.register(LavaWalkerConfig.class, JanksonConfigSerializer::new).getConfig();
